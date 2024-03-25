396684e460af5cc43052d9b26e3cf9b758e19839
Roswil Ascanio Aranguren <rascanio@kdu.cl>
2024-03-25 07:33:35 -0300

    add new file app

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ s3_client = boto3.client('s3', region_name='us-east-1')
 s3_client = boto3.client('s3')
 bucket_name = 'docdigi-1'
 
+def upload_file_to_s3(file):
+    try:
+        # Sube el archivo al bucket en la carpeta lang_pro
+        s3_client.upload_fileobj(file, bucket_name, f'lang_pro/{file.name}')
+        return True
+    except NoCredentialsError:
+        return False
+
 def get_latest_file_in_lang_pro():
     # Obtiene el archivo más reciente en la carpeta lang_pro
     response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix='lang_pro/')
@@ -64,6 +72,14 @@ def generate_presigned_url(bucket_name, object_name, expiration=3600):
 
 # Título de la aplicación
 st.title('Convertidor de Documentos a PDF')
+
+# Cargador de archivos
+uploaded_file = st.file_uploader("Elige un archivo para cargar y procesar")
+if uploaded_file is not None:
+    if upload_file_to_s3(uploaded_file):
+        st.success('Archivo cargado exitosamente.')
+    else:
+        st.error('Error al cargar el archivo. Asegúrate de que las credenciales y permisos son correctos.')
 
 # Obtiene el archivo más reciente de lang_pro
 latest_file = get_latest_file_in_lang_pro()
